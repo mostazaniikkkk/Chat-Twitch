667e64ba822628689574a1120bc119907d9bcd90
sergio valle hidalgo <ser.valle@alumnos.duoc.cl>
2021-06-25 19:03:47 -0400

    finish

--- test (nah que ver, lo estaba usando pah aprender javascript xD)/sumatest.js
+++
@@ -1,8 +0,0 @@
-function testing(){
-    var image = document.getElementById("img");
-    if(username.textContent == "mostazaniikkkk"){
-        function time(){
-            image.src = "http://pa1.narvii.com/7264/41d2cfa4e361dc78b2fd76cf4b4b5bdfbb19e091r1-200-200_00.gif",1000 * 10
-        }
-    }
-}
